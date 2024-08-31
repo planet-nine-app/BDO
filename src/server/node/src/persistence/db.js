ad79591b2d4a7b33969624616749fd0aa885c212
zach-planet-nine <zach@planetnine.app>
2024-08-30 20:14:31 -0700

    gitignore

--- src/server/node/src/persistence/db.js
+++ src/server/node/src/persistence/db.js
@@ -1,5 +1,5 @@
 import config from '../../config/local.js';
-import { createClient } from 'redis';
+import { createClient } from './client.js';
 import sessionless from 'sessionless-node';
 
 const client = await createClient()
@@ -8,22 +8,18 @@ const client = await createClient()
 
 const db = {
   getBDO: async (uuid, hash) => {
-console.log(uuid, hash);
     const bdo = await client.get(`bdo:${uuid}_${hash}`);
     const parsedBDO = JSON.parse(bdo);
-console.log(`parsedBDO: ${JSON.stringify(parsedBDO)}`);
-console.log(uuid);
     return parsedBDO;
   },
 
   putBDO: async (uuid, bdo, hash) => {
-console.log(uuid, bdo, hash);
     await client.set(`bdo:${uuid}_${hash}`, JSON.stringify(bdo));
     return bdo;
   },
 
   deleteBDO: async (uuid, hash) => {
-    const resp = await client.sendCommand(['DEL', `bdo:${uuid}_${hash}`]);
+    const resp = await client.del(`bdo:${uuid}_${hash}`);
 
     return true;
   },
