1e3fc1c9a5f6c4899995e8ca094526f5dcb52427
zach-planet-nine <zach@planetnine.app>
2025-02-22 23:29:13 -0800

    accept bigger bdos

--- src/server/node/bdo.js
+++ src/server/node/bdo.js
@@ -52,7 +52,7 @@ sessionless.generateKeys(sk, gk);
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json(limit: '10mb'}));
 
 app.use((req, res, next) => {
   const requestTime = +req.query.timestamp || +req.body.timestamp;
@@ -70,10 +70,16 @@ app.put('/user/create', async (req, res) => {
     const newBDO = body.bdo;
     const pub = body.public;
     const pubKey = body.pubKey;
+
+    const authBody = {
+      hash,
+      pubKey,
+      signature: body.signature
+    };
     
     const resp = await fetch(`${continuebeeURL}user/create`, {
       method: 'post',
-      body: JSON.stringify(body),
+      body: JSON.stringify(authBody),
       headers: {'Content-Type': 'application/json'}
     });
     
